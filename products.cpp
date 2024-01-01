3a4b38dab080db5a1e1842f539d9fe39ea0bbe0a
Adi Maulana <adi.maulana@pactindo.com>
2024-01-01 21:44:16 +0700

    update: validasi list product empty

--- products.cpp
+++ products.cpp
@@ -97,6 +97,9 @@ void displayProducts(bool holdScreen) {
 	
 	if (count == 0) {
 		printf("Produk Masih Kosong");	
+		
+	} else {
+		printf(" ID \t | Nama \t\t | Jumlah \t | Harga \t\n");
 	}
 	
 	// Open the file in read mode
@@ -104,8 +107,6 @@ void displayProducts(bool holdScreen) {
     if (fileProducts == NULL) {
         perror("Error opening file products");
     }
-    
-    printf(" ID \t | Nama \t\t | Jumlah \t | Harga \t\n");
 
     char row[MAX_ROW_LENGTH];
     int isFirstRow = 1;
@@ -165,20 +166,127 @@ void updateProducts() {
 	
 	displayProducts(0);
 	
-	printf("\n \t   Edit Produk");
-	printf("\n \t -----------------------------------------\n");
+	if (count != 0) {
+		
+		printf("\n \t   Edit Produk");
+		printf("\n \t -----------------------------------------\n");
+		
+		int targetId;
+	    printf("\nMasukkan ID Produk yang akan diupdate: ");
+	    scanf("%d", &targetId);
+	    
+	    if (targetId > 0 ) {
+	    	
+	        Product newProduct = getInputProduct(false);
+	    	
+		    FILE *tempFile; 
+		    char tempFilename[] = "temp_products.csv"; 
+		      
+		    // Open the original file in read mode
+		    fileProducts = fopen(fileProductsName, "r"); 
+		    if (fileProducts == NULL) {
+		        perror("Error opening file products"); 
+		    }
+		
+		    // Open a temporary file in write mode
+		    tempFile = fopen(tempFilename, "w");
+		    if (tempFile == NULL) {
+		        perror("Error opening temporary file");
+		        fclose(fileProducts); 
+		    }
+		
+		    // Read and process rows of data
+		    char row[MAX_ROW_LENGTH];
+		    int isFirstRow = 1;
+		    
+			// Read and loop through rows of data    
+		    while (fgets(row, sizeof(row), fileProducts) != NULL) {
+		    	
+		 		// Skip the first row (header)
+		        if (isFirstRow) {
+		        	fprintf(tempFile, "Id|Name|Quantity|Price\n");
+		            isFirstRow = 0;
+		            continue;
+				}
+				 
+		        char *data = strtok(row, "|");
+		 
+		        Product product;
+		
+		        // Parse and process each field
+		        int fieldCount = 0;
+		        while (data != NULL && fieldCount < MAX_FIELDS) {
+		            switch (fieldCount) {
+		                case 0:
+		                    product.id = atoi(data);
+		                    break;
+		                case 1:
+		                    strncpy(product.name, data, sizeof(product.name) - 1);
+		                    product.name[sizeof(product.name) - 1] = '\0'; // Ensure null-termination
+		                    break;
+		                case 2:
+		                    product.quantity = atoi(data);
+		                    break;
+		                case 3:
+		                    product.price = atoi(data);
+		                    break;
+		            }
+		
+		            // Get the next token
+		            data = strtok(NULL, "|");
+		
+		            fieldCount++;
+		        }
+		 
+		        if (isTargetRow(&product, targetId)) { 
+		            fprintf(tempFile, "%d|%s|%d|%.0f\n", targetId, newProduct.name, newProduct.quantity, newProduct.price);
+		            
+		        } else {
+		        	fprintf(tempFile, "%d|%s|%d|%.0f\n", product.id, product.name, product.quantity, product.price);
+				}
+		    }
+		
+		    // Close both files
+		    fclose(fileProducts);
+		    fclose(tempFile);
+		
+		    // Delete the original file
+		    if (remove(fileProductsName) != 0) {
+		        perror("Error deleting original file"); 
+		    }
+		    
+		    // Rename the temporary file to overwrite the original file
+		    if (rename(tempFilename, fileProductsName) != 0) {
+		        perror("Error renaming file"); 
+		    }
+		
+		    printf("\nID Produk %d berhasil diupdate", targetId);
+		    
+	    } else {
+	        printf("\nID Produk tidak valid.");
+	    }
+	}
 	
-	int targetId;
-    printf("\nMasukkan ID Produk yang akan diupdate: ");
-    scanf("%d", &targetId);
-    
-    if (targetId > 0 ) {
-    	
-        Product newProduct = getInputProduct(false);
-    	
-	    FILE *tempFile; 
+	printf("\nTekan ENTER untuk lanjut ");
+    getch();
+}
+
+void deleteProducts() {
+	
+	displayProducts(0);
+	
+	if (count != 0) {
+		
+		printf("\n \t   Hapus Produk");
+		printf("\n \t -----------------------------------------\n");
+		
+		FILE *tempFile; 
 	    char tempFilename[] = "temp_products.csv"; 
-	      
+	    
+	    int targetId; // ID of the row to delete
+	    printf("\nMasukkan ID Produk yang akan dihapus: ");
+	    scanf("%d", &targetId);
+	
 	    // Open the original file in read mode
 	    fileProducts = fopen(fileProductsName, "r"); 
 	    if (fileProducts == NULL) {
@@ -235,12 +343,9 @@ void updateProducts() {
 	            fieldCount++;
 	        }
 	 
-	        if (isTargetRow(&product, targetId)) { 
-	            fprintf(tempFile, "%d|%s|%d|%.0f\n", targetId, newProduct.name, newProduct.quantity, newProduct.price);
-	            
-	        } else {
-	        	fprintf(tempFile, "%d|%s|%d|%.0f\n", product.id, product.name, product.quantity, product.price);
-			}
+	        if (!isTargetRow(&product, targetId)) { 
+	            fprintf(tempFile, "%d|%s|%d|%.0f\n", product.id, product.name, product.quantity, product.price);
+	        }
 	    }
 	
 	    // Close both files
@@ -257,105 +362,9 @@ void updateProducts() {
 	        perror("Error renaming file"); 
 	    }
 	
-	    printf("\nID Produk %d berhasil diupdate", targetId);
-	    printf("\nTekan ENTER untuk lanjut ");
-    	
-    } else {
-        printf("\nID Produk tidak valid.");
-    }
-    getch();
-}
-
-void deleteProducts() {
+	    printf("\nID Produk %d berhasil dihapus", targetId);
+	}
 	
-	displayProducts(0);
-	
-	printf("\n \t   Hapus Produk");
-	printf("\n \t -----------------------------------------\n");
-	
-	FILE *tempFile; 
-    char tempFilename[] = "temp_products.csv"; 
-    
-    int targetId; // ID of the row to delete
-    printf("\nMasukkan ID Produk yang akan dihapus: ");
-    scanf("%d", &targetId);
-
-    // Open the original file in read mode
-    fileProducts = fopen(fileProductsName, "r"); 
-    if (fileProducts == NULL) {
-        perror("Error opening file products"); 
-    }
-
-    // Open a temporary file in write mode
-    tempFile = fopen(tempFilename, "w");
-    if (tempFile == NULL) {
-        perror("Error opening temporary file");
-        fclose(fileProducts); 
-    }
-
-    // Read and process rows of data
-    char row[MAX_ROW_LENGTH];
-    int isFirstRow = 1;
-    
-	// Read and loop through rows of data    
-    while (fgets(row, sizeof(row), fileProducts) != NULL) {
-    	
- 		// Skip the first row (header)
-        if (isFirstRow) {
-        	fprintf(tempFile, "Id|Name|Quantity|Price\n");
-            isFirstRow = 0;
-            continue;
-		}
-		 
-        char *data = strtok(row, "|");
- 
-        Product product;
-
-        // Parse and process each field
-        int fieldCount = 0;
-        while (data != NULL && fieldCount < MAX_FIELDS) {
-            switch (fieldCount) {
-                case 0:
-                    product.id = atoi(data);
-                    break;
-                case 1:
-                    strncpy(product.name, data, sizeof(product.name) - 1);
-                    product.name[sizeof(product.name) - 1] = '\0'; // Ensure null-termination
-                    break;
-                case 2:
-                    product.quantity = atoi(data);
-                    break;
-                case 3:
-                    product.price = atoi(data);
-                    break;
-            }
-
-            // Get the next token
-            data = strtok(NULL, "|");
-
-            fieldCount++;
-        }
- 
-        if (!isTargetRow(&product, targetId)) { 
-            fprintf(tempFile, "%d|%s|%d|%.0f\n", product.id, product.name, product.quantity, product.price);
-        }
-    }
-
-    // Close both files
-    fclose(fileProducts);
-    fclose(tempFile);
-
-    // Delete the original file
-    if (remove(fileProductsName) != 0) {
-        perror("Error deleting original file"); 
-    }
-    
-    // Rename the temporary file to overwrite the original file
-    if (rename(tempFilename, fileProductsName) != 0) {
-        perror("Error renaming file"); 
-    }
-
-    printf("\nID Produk %d berhasil dihapus", targetId);
     printf("\nTekan ENTER untuk lanjut ");
     getch();
 }
